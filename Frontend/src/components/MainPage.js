213915ca36cbb0e74c33e0264661dce2cd686844
KuraiZoltan <kz1022@gmail.com>
2023-03-07 15:47:46 +0100

    Main page component addition

---
+++ Frontend/src/components/MainPage.js
@@ -0,0 +1,20 @@
+
+
+export function Main() {
+    return (
+        <div className="main-page-content">
+            <div className="title-content">
+                <h1>Boost your rank with Me!</h1>
+            </div>
+            <div className="game-select">
+                <a href="/league-of-legends" className="game-option">
+                    <button className="select-game-btn">League of Legends</button>
+                </a>
+                <a href="/valorant" className="game-option">
+                    <button className="select-game-btn">Valorant</button>
+                </a>
+                
+            </div>
+        </div>
+    )
+}
