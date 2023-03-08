0fc83fe3a9c5b974e90cfed3d29abb89bff6c23e
KuraiZoltan <kz1022@gmail.com>
2023-03-08 16:23:49 +0100

    Challenger removal, class addition to rank buttons

--- Frontend/src/components/LeagueOfLegends.js
+++ Frontend/src/components/LeagueOfLegends.js
@@ -25,15 +25,14 @@ export function LeagueOfLegends() {
     const rankSelectFromBlock = (
         <div className="select-from-buttons">
             <h2 className="rank-form-title">Select your current rank here!</h2>
-            <button onClick={() => getSelectRankFrom("Iron")}><img src="https://boostroyal.no/assets/images/divisions/lol/iron.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Bronze")}><img src="https://boostroyal.no/assets/images/divisions/lol/bronze.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Silver")}><img src="https://boostroyal.no/assets/images/divisions/lol/silver.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Gold")}><img src="https://boostroyal.no/assets/images/divisions/lol/gold.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Platinum")}><img src="https://boostroyal.no/assets/images/divisions/lol/platinum.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Diamond")}><img src="https://boostroyal.no/assets/images/divisions/lol/diamond.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Master")}><img src="https://boostroyal.no/assets/images/divisions/lol/master.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Grandmaster")}><img src="https://boostroyal.no/assets/images/divisions/lol/grandmaster.png"></img></button>
-            <button onClick={() => getSelectRankFrom("Challenger")}><img src="https://boostroyal.no/assets/images/divisions/lol/challenger.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Iron")}><img src="https://boostroyal.no/assets/images/divisions/lol/iron.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Bronze")}><img src="https://boostroyal.no/assets/images/divisions/lol/bronze.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Silver")}><img src="https://boostroyal.no/assets/images/divisions/lol/silver.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Gold")}><img src="https://boostroyal.no/assets/images/divisions/lol/gold.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Platinum")}><img src="https://boostroyal.no/assets/images/divisions/lol/platinum.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Diamond")}><img src="https://boostroyal.no/assets/images/divisions/lol/diamond.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Master")}><img src="https://boostroyal.no/assets/images/divisions/lol/master.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankFrom("Grandmaster")}><img src="https://boostroyal.no/assets/images/divisions/lol/grandmaster.png"></img></button>
             <div>
                 <select className="form-select" onChange={(e) => { let selectedRank = e.target.value; setDropdownOptionFromState(selectedRank) }}>
                     <option value="0">Nothing selected</option>
@@ -53,15 +52,14 @@ export function LeagueOfLegends() {
     const rankSelectToBlock = (
         <div className="select-to-buttons">
             <h2 className="rank-form-title">Select your desired rank here!</h2>
-            <button onClick={() => getSelectRankTo("Iron")}><img src="https://boostroyal.no/assets/images/divisions/lol/iron.png"></img></button>
-            <button onClick={() => getSelectRankTo("Bronze")}><img src="https://boostroyal.no/assets/images/divisions/lol/bronze.png"></img></button>
-            <button onClick={() => getSelectRankTo("Silver")}><img src="https://boostroyal.no/assets/images/divisions/lol/silver.png"></img></button>
-            <button onClick={() => getSelectRankTo("Gold")}><img src="https://boostroyal.no/assets/images/divisions/lol/gold.png"></img></button>
-            <button onClick={() => getSelectRankTo("Platinum")}><img src="https://boostroyal.no/assets/images/divisions/lol/platinum.png"></img></button>
-            <button onClick={() => getSelectRankTo("Diamond")}><img src="https://boostroyal.no/assets/images/divisions/lol/diamond.png"></img></button>
-            <button onClick={() => getSelectRankTo("Master")}><img src="https://boostroyal.no/assets/images/divisions/lol/master.png"></img></button>
-            <button onClick={() => getSelectRankTo("Grandmaster")}><img src="https://boostroyal.no/assets/images/divisions/lol/grandmaster.png"></img></button>
-            <button onClick={() => getSelectRankTo("Challenger")}><img src="https://boostroyal.no/assets/images/divisions/lol/challenger.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Iron")}><img src="https://boostroyal.no/assets/images/divisions/lol/iron.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Bronze")}><img src="https://boostroyal.no/assets/images/divisions/lol/bronze.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Silver")}><img src="https://boostroyal.no/assets/images/divisions/lol/silver.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Gold")}><img src="https://boostroyal.no/assets/images/divisions/lol/gold.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Platinum")}><img src="https://boostroyal.no/assets/images/divisions/lol/platinum.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Diamond")}><img src="https://boostroyal.no/assets/images/divisions/lol/diamond.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Master")}><img src="https://boostroyal.no/assets/images/divisions/lol/master.png"></img></button>
+            <button className="league-rank-button" onClick={() => getSelectRankTo("Grandmaster")}><img src="https://boostroyal.no/assets/images/divisions/lol/grandmaster.png"></img></button>
             <div>
                 <select className="form-select" onChange={(e) => { let selectedRank = e.target.value; setDropdownOptionToState(selectedRank) }}>
                     <option value="0">Nothing selected</option>
