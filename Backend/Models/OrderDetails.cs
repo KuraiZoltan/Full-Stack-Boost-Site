d79eaa78e5ff3875d13925b14cb76ffe0f75d578
KuraiZoltan <kz1022@gmail.com>
2023-03-09 13:22:07 +0100

    Region and rank point addition in model

--- Backend/Models/OrderDetails.cs
+++ Backend/Models/OrderDetails.cs
@@ -7,6 +7,7 @@
         public string CurrentRankPoints { get; set; }
         public string OrderedRank { get; set; }
         public string OrderedRankLevel { get; set;}
+        public string SelectedRegion { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
