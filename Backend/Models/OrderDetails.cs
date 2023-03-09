4d79c2184184ecab49e4baf8b66135e034c20ea7
KuraiZoltan <kz1022@gmail.com>
2023-03-09 12:16:48 +0100

    Order details model update with rank points

--- Backend/Models/OrderDetails.cs
+++ Backend/Models/OrderDetails.cs
@@ -4,6 +4,7 @@
     {
         public string CurrentRank { get; set; }
         public string CurrentRankLevel { get; set; }
+        public string CurrentRankPoints { get; set; }
         public string OrderedRank { get; set; }
         public string OrderedRankLevel { get; set;}
         public string FirstName { get; set; }
