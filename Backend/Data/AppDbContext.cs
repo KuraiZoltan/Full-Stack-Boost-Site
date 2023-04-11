dc56b407badd9dfaf26ff365e970770c61f6670f
KuraiZoltan <kz1022@gmail.com>
2023-04-11 16:45:27 +0200

    Database context addition

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmailSender.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailSender.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<OrderDetails> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
