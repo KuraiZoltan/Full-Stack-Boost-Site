69697bd6f140e201c28f1438b2356628e51211a3
KuraiZoltan <kz1022@gmail.com>
2023-04-11 16:45:10 +0200

    Registration method implementation

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using EmailSender.Data;
+using EmailSender.Models.Credentials;
+using EmailSender.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSender.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("registrate")]
+        public async Task RegistrateUser([FromBody]RegistrationCredentials registrationCredentials)
+        {
+            await _userService.AddUser(registrationCredentials);
+        }
+    }
+}
