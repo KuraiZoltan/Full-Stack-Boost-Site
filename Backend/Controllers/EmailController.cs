f403e4d637bfd9883db7a766525b865aa8b6b6e2
KuraiZoltan <kz1022@gmail.com>
2023-04-11 14:58:52 +0200

    Refactoring, email service creation

--- Backend/Controllers/EmailController.cs
+++ Backend/Controllers/EmailController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MailKit.Net.Smtp;
-using MimeKit;
 using EmailSender.Models;
+using EmailSender.Services;
 
 namespace EmailSender.Controllers
 {
@@ -9,32 +8,16 @@ namespace EmailSender.Controllers
     [ApiController]
     public class EmailController : ControllerBase
     {
+        private EmailService _emailService;
+        public EmailController(EmailService emailService) 
+        {
+            _emailService = emailService;
+        }
+
         [HttpPost]
         public IActionResult SendEmail([FromBody] OrderDetails orderDetails)
         {
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("fakebboost@gmail.com"));
-            email.To.Add(MailboxAddress.Parse(orderDetails.Email));
-            email.Subject = "FB Order Confirmation";
-            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text =
-                $"<h1>FB Order Details</h1> " +
-                $"<p><b>First Name:</b> {orderDetails.FirstName}</p> " +
-                $"<p><b>Last Name:</b> {orderDetails.LastName}</p> " +
-                $"<p><b>Game:</b> {orderDetails.GameName}</p> " +
-                $"<p><b>Region:</b> {orderDetails.SelectedRegion}</p>" +
-                $"<p><b>Boost From:</b> {orderDetails.CurrentRank} {orderDetails.CurrentRankLevel} {orderDetails.CurrentRankPoints}</p> " +
-                $"<p><b>Boost To:</b> {orderDetails.OrderedRank} {orderDetails.OrderedRankLevel}</p>" +
-                "<h2>Thank You for your order, FB will contact you soon!</h2>"
-            };
-
-            using var smtp = new SmtpClient();
-            smtp.Connect("smtp.gmail.com", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
-            smtp.Authenticate("fakebboost@gmail.com", "gconlzshnpusjupa");
-            smtp.Send(email);
-            smtp.Disconnect(true);
-
+            _emailService.SendEmail(orderDetails);
             return Ok();
         }
     }
---
+++ Backend/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmailSender.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailSender.Services
+{
+    public class EmailService
+    {
+        public void SendEmail(OrderDetails orderDetails)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("fakebboost@gmail.com"));
+            email.To.Add(MailboxAddress.Parse(orderDetails.Email));
+            email.Subject = "FB Order Confirmation";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text =
+                $"<h1>FB Order Details</h1> " +
+                $"<p><b>First Name:</b> {orderDetails.FirstName}</p> " +
+                $"<p><b>Last Name:</b> {orderDetails.LastName}</p> " +
+                $"<p><b>Game:</b> {orderDetails.GameName}</p> " +
+                $"<p><b>Region:</b> {orderDetails.SelectedRegion}</p>" +
+                $"<p><b>Boost From:</b> {orderDetails.CurrentRank} {orderDetails.CurrentRankLevel} {orderDetails.CurrentRankPoints}</p> " +
+                $"<p><b>Boost To:</b> {orderDetails.OrderedRank} {orderDetails.OrderedRankLevel}</p>" +
+                "<h2>Thank You for your order, FB will contact you soon!</h2>"
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
+            smtp.Authenticate("fakebboost@gmail.com", "gconlzshnpusjupa");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
