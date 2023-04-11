5df8139b8901a6e5ac7a115824511ad5bc44021a
KuraiZoltan <kz1022@gmail.com>
2023-04-11 16:46:19 +0200

    Add user method implementation

---
+++ Backend/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using EmailSender.Data;
+using EmailSender.Models;
+using EmailSender.Models.Credentials;
+using Microsoft.AspNetCore.Identity;
+
+namespace EmailSender.Services
+{
+    public class UserService
+    {
+        private PasswordHasher<User> PasswordHasher { get; }
+        private AppDbContext _context { get; set; }
+        public UserService() { }
+
+        public async Task AddUser(RegistrationCredentials registrationCredentials)
+        {
+            User user = new User()
+            {
+                Username = registrationCredentials.Username,
+                Email = registrationCredentials.Email,
+            };
+            user.HashedPassword = PasswordHasher.HashPassword(user, registrationCredentials.Password);
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
