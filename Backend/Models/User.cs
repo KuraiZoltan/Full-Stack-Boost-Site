ca2f8cc8b803d6d3ba16bbbf5eb8eaf96801018e
KuraiZoltan <kz1022@gmail.com>
2023-04-12 13:39:49 +0200

    User and credential update

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -6,6 +6,8 @@ namespace EmailSender.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string HashedPassword { get; set; }
