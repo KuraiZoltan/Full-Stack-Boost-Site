938bebd64ead374f4ac04abf33784d4079fe8b74
KuraiZoltan <kz1022@gmail.com>
2023-04-19 14:32:33 +0200

    Order saving and returning all orders method implementations

---
+++ Backend/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using EmailSender.Data;
+using EmailSender.Models;
+
+namespace EmailSender.Services
+{
+    public class OrderService
+    {
+        private AppDbContext _context { get; set; }
+
+        public OrderService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveOrder(OrderDetails orderDetails)
+        {
+            _context.Orders.Add(orderDetails);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<OrderDetails> GetAllOrders()
+        {
+            return _context.Orders.ToList();
+        }
+    }
+}
