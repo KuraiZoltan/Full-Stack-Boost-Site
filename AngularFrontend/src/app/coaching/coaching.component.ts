e126f6a06f8d827ec2aabc21625827db1cca9e8a
KuraiZoltan <kz1022@gmail.com>
2023-05-03 19:11:14 +0200

    Input validation

--- AngularFrontend/src/app/coaching/coaching.component.ts
+++ AngularFrontend/src/app/coaching/coaching.component.ts
@@ -25,42 +25,74 @@ export class CoachingComponent {
   public rankOptions = ["Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master"]
   public lpRangeOptions = ["0-20", "21-40", "41-60", "61-80", "81-100"]
   public laneOptions = ["Top", "Middle", "Bottom", "Support", "Jungle"]
+  public incorrectData = false
+  public incorrectOption = false
 
   constructor(private http: HttpClient) { }
 
   saveLane(event: any) {
-    this.order.Lane = event.currentTarget.value
+    if (event.currentTarget.value === '0') {
+      this.incorrectOption = true
+    } else {
+      this.incorrectOption = false
+      this.order.Lane = event.currentTarget.value
+    }
   }
 
   saveRank(event: any) {
-    this.rank.Rank = event.currentTarget.value
+    if (event.currentTarget.value === '0') {
+      this.incorrectOption = true
+    } else {
+      this.incorrectOption = false
+      this.rank.Rank = event.currentTarget.value
+    }
   }
 
   saveRankLevel(event: any) {
-    this.rank.Rank += ` ${event.currentTarget.value}`
+    if (event.currentTarget.value === '0') {
+      this.incorrectOption = true
+    } else {
+      this.incorrectOption = false
+      this.rank.Rank += ` ${event.currentTarget.value}`
+    }
   }
 
   saveLpRange(event: any) {
-    this.rank.Lp = event.currentTarget.value
+    if (event.currentTarget.value === '0') {
+      this.incorrectOption = true
+    } else {
+      this.incorrectOption = false
+      this.rank.Lp = event.currentTarget.value
+    }
   }
 
   saveSessionCount(event: any) {
-    this.order.SessionCount = event.currentTarget.value
+    if (event.currentTarget.value === '0') {
+      this.incorrectOption = true
+    } else {
+      this.incorrectOption = false
+      this.order.SessionCount = event.currentTarget.value
+    }
   }
 
   onSubmit(form: NgForm) {
-    let payload = {
-      UserId: parseInt(sessionStorage.getItem("user_id") as string),
-      Email: this.order.Email,
-      DiscordName: this.order.DiscordName,
-      PlayedLane: this.order.Lane,
-      Rank: this.order.Rank = `${this.rank.Rank} ${this.rank.Lp}`,
-      SessionCount: this.order.SessionCount,
-      SessionsFinished: "0 hours"
+    if (this.order.Lane !== "" && this.order.SessionCount !== "" && this.rank.Rank !== "" && this.rank.Lp !== "") {
+      this.incorrectData = false
+      let payload = {
+        UserId: parseInt(sessionStorage.getItem("user_id") as string),
+        Email: this.order.Email,
+        DiscordName: this.order.DiscordName,
+        PlayedLane: this.order.Lane,
+        Rank: this.order.Rank = `${this.rank.Rank} ${this.rank.Lp}`,
+        SessionCount: this.order.SessionCount,
+        SessionsFinished: "0 hours"
+      }
+      const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${sessionStorage.getItem("jwt")}` }
+      const body = JSON.stringify(payload)
+      this.http.post("https://localhost:7196/Email/sendCoachingEmail", body, { 'headers': headers }).subscribe(r => { console.log(r) })
+    } else {
+      this.incorrectData = true
     }
-    const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${sessionStorage.getItem("jwt")}` }
-    const body = JSON.stringify(payload)
-    this.http.post("https://localhost:7196/Email/sendCoachingEmail", body, { 'headers': headers }).subscribe(r => { console.log(r) })
   }
 }
 
