c5c538e2c73f1e8ff7b729799e0c941614b3285e
KuraiZoltan <kz1022@gmail.com>
2023-05-30 10:49:24 +0200

    Registration page rework with registration form component, new header and about us component

--- AngularFrontend/src/app/registration-page/registration-page.component.ts
+++ AngularFrontend/src/app/registration-page/registration-page.component.ts
@@ -7,35 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration-page.component.css']
 })
 export class RegistrationPageComponent {
-  constructor(private http: HttpClient) { }
-
-  submit(userForm: any) {
-    let payload: User = {
-      DiscordName: `${userForm.form.controls["discord-name"].value}#${userForm.form.controls["discord-tag"].value}`,
-      Username: userForm.form.controls["username"].value,
-      Email: userForm.form.controls["email"].value,
-      Password: userForm.form.controls["password"].value,
-      PasswordConfirmation: userForm.form.controls["confirm-password"].value
-    }
-    const headers = { 'Content-Type': 'application/json' }
-    const body = JSON.stringify(payload)
-    return this.http.post("https://localhost:7196/User/registrate", body, { 'headers': headers }).subscribe(r => { this.checkResponse(r) })
-  }
-
-  checkResponse(result: any) {
-    if (result.statusCode === 200) {
-      window.location.href = '/login'
-      alert("Registered successfully")
-    } else {
-      alert("Something went wrong! Check your email and password!")
-    }
-  }
+  
 }
 
-interface User {
-  DiscordName: string;
-  Username: string;
-  Email: string;
-  Password: string;
-  PasswordConfirmation: string;
-}
