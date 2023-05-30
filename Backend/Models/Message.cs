40684a42962d4b6132c50ee892ecc0e7befc3270
KuraiZoltan <kz1022@gmail.com>
2023-05-30 11:26:28 +0200

    Message model implementation

---
+++ Backend/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailSender.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Discord { get; set; }
+        public string MessageText { get; set; }
+    }
+}
