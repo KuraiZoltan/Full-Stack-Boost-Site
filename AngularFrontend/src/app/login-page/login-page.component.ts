0bd197b2aaee1b347b6fd39399fc49a43e7524db
KuraiZoltan <kz1022@gmail.com>
2023-05-30 10:38:53 +0200

    Login page rework with login component, new header and about us component

--- AngularFrontend/src/app/login-page/login-page.component.ts
+++ AngularFrontend/src/app/login-page/login-page.component.ts
@@ -8,29 +8,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-  invalidLogin: boolean | undefined;
-
-  constructor(private router: Router, private http: HttpClient) { }
-
-  login(loginCredentials: any) {
-    let payload = {
-      Username: loginCredentials.form.controls["username"].value,
-      Password: loginCredentials.form.controls["password"].value
-    }
-    const headers = { 'Content-Type': 'application/json' }
-    const body = JSON.stringify(payload)
-    this.http.post("https://localhost:7196/User/loginUser", body, { "headers": headers, withCredentials: true, observe: 'response' as 'response' })
-      .subscribe(response => {
-        const token = (<any>response.body)
-        sessionStorage.setItem("email", token.email)
-        sessionStorage.setItem("discord_name", token.discordName)
-        sessionStorage.setItem("user_id", token.userId)
-        sessionStorage.setItem("role", token.role)
-        sessionStorage.setItem("username", token.username)
-        this.invalidLogin = false
-        window.location.href = "/"
-      }, err => {
-        this.invalidLogin = true
-      })
-  }
+  
 }
