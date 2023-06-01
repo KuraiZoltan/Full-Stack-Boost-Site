cff465e462d3815b64125c028c26d819c318693b
KuraiZoltan <kz1022@gmail.com>
2023-06-01 11:15:50 +0200

    Order interface implementation

---
+++ AngularFrontend/src/app/interfaces/Order.ts
@@ -0,0 +1,13 @@
+export interface Order {
+  id: number;
+  currentRank: string;
+  currentRankLevel: string;
+  currentRankPoints: string;
+  orderedRank: string;
+  orderedRankLevel: string;
+  gameName: string;
+  username: string;
+  email: string;
+  selectedRegion: string;
+  status: string;
+}
