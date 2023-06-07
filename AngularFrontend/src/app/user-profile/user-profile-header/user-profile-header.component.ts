e58fb10897d674becf2659dfe144a252ebb633e2
KuraiZoltan <kz1022@gmail.com>
2023-06-07 17:28:49 +0200

    Header component implementation

---
+++ AngularFrontend/src/app/user-profile/user-profile-header/user-profile-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-header',
+  templateUrl: './user-profile-header.component.html',
+  styleUrls: ['../user-profile.component.css']
+})
+export class UserProfileHeaderComponent {
+
+}
