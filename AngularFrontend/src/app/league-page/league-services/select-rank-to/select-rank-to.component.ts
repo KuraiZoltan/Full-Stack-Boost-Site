62ccfd34665a677f810b6d61b41bc0a8500cba06
KuraiZoltan <kz1022@gmail.com>
2023-06-14 14:36:27 +0200

    Ranked queue type selection implementation

--- AngularFrontend/src/app/league-page/league-services/select-rank-to/select-rank-to.component.ts
+++ AngularFrontend/src/app/league-page/league-services/select-rank-to/select-rank-to.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Order } from '../../../interfaces/Order';
 import { Rank } from '../../../interfaces/Rank';
 
@@ -12,21 +12,33 @@ export class SelectRankToComponent {
   @Input() ranks!: Rank[];
   @Input() rankOptions!: string[];
   @Input() rankLevels!: string[];
-
-  constructor() { }
+   
+  public isSoloDuo: boolean = false;
+  public isFlex: boolean = false;
 
   saveDesiredRank(OrderedRank: any) {
-    this.order.orderedRank = OrderedRank
-    console.log(this.order)
+    this.order.orderedRank = OrderedRank;
   }
 
   saveDesiredRankLevel(level: any) {
-    this.order.orderedRankLevel = level
-    console.log(event)
+    this.order.orderedRankLevel = level;
   }
 
   saveRegion(event: any) {
-    this.order.selectedRegion = event.currentTarget.value
+    this.order.selectedRegion = event.currentTarget.value;
+  }
+
+  saveSoloDuoInput() {
+    this.isFlex = false
+    this.isSoloDuo = true;
+    this.order.rankedType = "Solo/Duo"
+    console.log(this.order)
+  }
+
+  saveFlexInput() {
+    this.isSoloDuo = false
+    this.isFlex = true;
+    this.order.rankedType = "Flex"
     console.log(this.order)
   }
 }
